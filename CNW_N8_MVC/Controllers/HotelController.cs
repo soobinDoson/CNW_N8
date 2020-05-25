f5ed6b5715dd7142a0194b11bd5db1accec9baf7
hothang2407@gmail.com <hothang2407@gmail.com>
2020-05-25 10:38:10 +0700

    hotel list

--- CNW_N8_MVC/Controllers/HotelController.cs
+++ CNW_N8_MVC/Controllers/HotelController.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+using CNW_N8_MVC.Models;
 
 namespace CNW_N8_MVC.Controllers
 {
     public class HotelController : Controller
     {
-        // GET: Hotel
+        private Model1 context = new Model1();
+
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult List()
         {
-            return View();
+            dynamic model = new ExpandoObject();
+            model.hotels = context.hotels.Where(x => x.id != 0).ToList();
+            return View(model);
         }
         public ActionResult Detail()
         {
