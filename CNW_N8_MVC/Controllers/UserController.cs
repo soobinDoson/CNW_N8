088bc2f63c1fd28f873289779fb78e2ae240e32c
TUNG <portal25101999@gmail.com>
2020-05-30 12:13:23 +0700

    register and login

--- CNW_N8_MVC/Controllers/UserController.cs
+++ CNW_N8_MVC/Controllers/UserController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using CNW_N8_MVC.Models;
+
 namespace CNW_N8_MVC.Controllers
 {
     public class UserController : Controller
     {
+
+        private Model1 context = new Model1();
         // GET: User
         public ActionResult Index()
         {
@@ -16,6 +20,28 @@ namespace CNW_N8_MVC.Controllers
         
         public ActionResult Login()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(user user, string ReturnUrl)
+        {
+            var result = context.users.Where(a => a.username.Equals(user.username) &&
+                                                       a.password.Equals(user.password)).FirstOrDefault();
+            if (result != null)
+            {
+                Session["Login"] = user;
+                if (string.IsNullOrEmpty(ReturnUrl))
+                {
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return Redirect(ReturnUrl);
+                }
+
+            }
             return View();
         }
 
