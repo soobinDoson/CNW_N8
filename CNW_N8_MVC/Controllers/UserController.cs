0482b00b90fdf7ae117d1de3a220c2805ee79064
nqthai309 <nqthai199@gmail.com>
2020-05-30 14:37:06 +0700

    fix

--- CNW_N8_MVC/Controllers/UserController.cs
+++ CNW_N8_MVC/Controllers/UserController.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 using CNW_N8_MVC.Models;
 
 namespace CNW_N8_MVC.Controllers
 {
     public class UserController : Controller
     {
-
-        private Model1 context = new Model1();
+        Model1 context = new Model1();
+        public static int idAccount=0;
+        public static string userName=null;
         // GET: User
         public ActionResult Index()
         {
@@ -22,27 +22,31 @@ namespace CNW_N8_MVC.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Login(user user, string ReturnUrl)
+        
+        public ActionResult Logout()
         {
-            var result = context.users.Where(a => a.username.Equals(user.username) &&
-                                                       a.password.Equals(user.password)).FirstOrDefault();
-            if (result != null)
+            Session["Login"] = null;
+            idAccount = 0;
+            userName = null;
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public ActionResult LoginCenter(user acc)
+        {
+            var result = context.users.Where(a => (a.username == acc.username && a.password == acc.password)).FirstOrDefault();
+            if(result != null)
             {
-                Session["Login"] = user;
-                if (string.IsNullOrEmpty(ReturnUrl))
-                {
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    return Redirect(ReturnUrl);
-                }
-
+                Session["Login"] = acc;
+                idAccount = result.id;
+                userName = result.username.ToString();
+                ViewData["username"] = result.username.ToString();
+                return RedirectToAction("Index", "Home");
             }
-            return View();
+            else
+            {
+                return View("Login");
+            }
+            
         }
 
         public ActionResult Register()
@@ -50,14 +54,111 @@ namespace CNW_N8_MVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RegisterCenter(user acc)
+        {
+            var result = context.users.Where(a => (a.username == acc.username)).FirstOrDefault();
+            if(result != null)
+            {
+                //Tài Khoản bị trùng//
+                return View("Register");
+
+            }
+            else
+            {
+                //Tài Khoản ko bị trùng//
+                try
+                {
+                    user newAcc = new user();
+                    newAcc.username = acc.username;
+                    newAcc.password = acc.password;
+                    newAcc.role_id = 1;
+                    context.users.Add(newAcc);
+                    context.SaveChanges();
+                    return View("Login");
+                }
+                catch
+                {
+                    return View();
+                }
+                
+            }
+            
+        }
+
         public ActionResult Config()
         {
-            return View();
+            if (Session["Login"] == null)
+            {
+                return View("Login");
+            }
+            else
+            {
+                var acc = context.users.Find(idAccount);
+                ViewData["username"] = userName;
+                ViewData["acc"] = acc;
+                return View();
+            }
+            
         }
 
         public ActionResult Booking()
         {
+            if(Session["Login"] == null)
+            {
+                return View("Login");
+            }
+            ViewData["username"] = userName;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult UserChanges(user acc)
+        {
+            var obj = context.users.Find(idAccount);
+            obj.full_name = acc.full_name;
+            obj.phone = acc.phone;
+            obj.email = acc.email;
+            obj.address = acc.address;
+
+            context.SaveChanges();
+
+            return RedirectToAction("Config", "User");
+        }
+
+        [HttpGet]
+        public ActionResult PasswordChanges(string password, string newPassword1, string newPassword2)
+        {
+            var obj = context.users.Find(idAccount);
+            if (password == "" || newPassword1 == "" || newPassword2 == "")
+            {
+                //Điền Thiếu thông tin//
+                return RedirectToAction("Config", "User");
+            }
+            else
+            {
+              
+                if(obj.password == password)
+                {
+                    if (newPassword1 == newPassword2)
+                    {
+                        obj.password = newPassword1;
+                        context.SaveChanges();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        //Thông tin chưa chính xác
+                        return RedirectToAction("Config", "User");
+                    }
+                }
+                else
+                {
+                    //Thông tin chưa chính xác
+                    return RedirectToAction("Config", "User");
+                }
+            }
+            
         }
     }
 }
