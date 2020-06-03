0c1262a0f5679f30c5756a6c1bb20ced93cf6e5e
TUNG <portal25101999@gmail.com>
2020-06-03 16:47:42 +0700

    Update .gitignore

---
+++ CNW_N8_MVC/Controllers/Backend/BackendController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNW_N8_MVC.Controllers.Backend
+{
+    public class BackendController : Controller
+    {
+        // GET: Backend
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CNW_N8_MVC/Controllers/Backend/BackendUserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Dynamic;
+
+using CNW_N8_MVC.Models;
+
+namespace CNW_N8_MVC.Controllers.Backend
+{
+    public class BackendUserController : Controller
+    {
+        private Model1 context = new Model1();
+        // GET: User
+        public ActionResult List()
+        {
+            dynamic model = new ExpandoObject();
+            model.users = context.users.Where(x => x.id != 0).ToList();
+            return View(model);
+        }
+
+    }
+}
