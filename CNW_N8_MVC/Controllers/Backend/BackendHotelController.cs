fbdad34fb42a4eb6fcd30c02a73581eee7cbd221
hothang2407@gmail.com <hothang2407@gmail.com>
2020-06-10 14:00:41 +0700

    BE backendHotel

--- CNW_N8_MVC/Controllers/Backend/BackendHotelController.cs
+++ CNW_N8_MVC/Controllers/Backend/BackendHotelController.cs
@@ -12,6 +12,7 @@ namespace CNW_N8_MVC.Controllers.Backend
     public class BackendHotelController : BaseController
     {
         private Model1 context = new Model1();
+        static int id_Old;
         // GET: BackendHotel
         public ActionResult List()
         {
@@ -19,5 +20,152 @@ namespace CNW_N8_MVC.Controllers.Backend
             model.hotels = context.hotels.Where(x => x.id != 0).ToList();
             return View(model);
         }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit(string id)
+        {
+            int a;
+            bool check = int.TryParse(id.ToString(), out a);
+            if(check == true)
+            {
+                var model = context.hotels.Find(a);
+                if(model == null)
+                {
+                    return RedirectToAction("List", "BackendHotel");
+                }
+                else
+                {
+                    id_Old = a;
+                    var listLocations = context.locations.Where(l => l.location_name != null).ToList();
+                    ViewData["listLocations"] = listLocations;
+                    return View(model);
+                }
+            }
+            else
+            {
+                return RedirectToAction("List", "BackendHotel");
+            }
+        }
+        public int checkAddHotel(string location_id,string hotel_name,string description,string more_imformation,string price,string sell_price)
+        {
+            if (location_id == "" || hotel_name == "" || description == "" || more_imformation == "" || price == "" || sell_price == "")
+            {
+                return -1;
+            }
+            else
+            {
+                int a = int.Parse(location_id);
+                //  var listHotel = context.hotels.Where(h => h.location.id == int.Parse(location_id)).ToList();
+                var listHotel = context.hotels.Where(h => h.location_id == a).ToList();
+
+                foreach(var it in listHotel)
+                {
+                    if(it.hotel_name == hotel_name)
+                    {
+                        return 0;
+                    }
+                }
+                return 1;
+
+            }
+        }
+        [HttpPost]
+        public ActionResult AddHotel(hotel hotel)
+        {
+            hotel.image_url = "/Content/img/Group 68.png";
+            hotel.detail_header_image_url = "/Content/img/hotel-detail.jpg";
+            hotel.more_imformation_image_url = "/Content/img/Group 71.png";
+            context.hotels.Add(hotel);
+            context.SaveChanges();
+            return RedirectToAction("List", "BackendHotel");
+        }
+
+
+        public ActionResult DeleteHotel(string id)
+        {
+            int a;
+            bool check = int.TryParse(id.ToString(), out a);
+            if(check == true)
+            {
+                var result = context.hotels.Find(a);
+                if(result == null)
+                {
+                    return RedirectToAction("List", "BackendHotel");
+                }
+                else
+                {
+                    context.hotels.Remove(result);
+                    context.SaveChanges();
+                    return RedirectToAction("List", "BackendHotel");
+
+                }
+            }
+            else
+            {
+                return RedirectToAction("List", "BackendHotel");
+            }
+        }
+
+        public int checkEditHotel(string location_id, string hotel_name, string description, string more_imformation, string price, string sell_price)
+        {
+            if (location_id == "" || hotel_name == "" || description == "" || more_imformation == "" || price == "" || sell_price == "")
+            {
+                return -1;
+            }
+            else
+            {
+                int a = int.Parse(location_id);
+                var id_oldHotel = context.hotels.Find(id_Old);
+                if(a != id_oldHotel.location_id)
+                {
+                    var listHotel = context.hotels.Where(h => h.location_id == a).ToList();
+
+                    foreach (var it in listHotel)
+                    {
+                        if (it.hotel_name == hotel_name)
+                        {
+                            return 0;
+                        }
+                    }
+                    return 1;
+                }
+                return 1;
+                //else
+                //{
+                //    int a1 = int.Parse(location_id);
+                //    //  var listHotel = context.hotels.Where(h => h.location.id == int.Parse(location_id)).ToList();
+                //    var listHotel = context.hotels.Where(h => h.location_id == a1).ToList();
+
+                //    foreach (var it in listHotel)
+                //    {
+                //        if (it.hotel_name == hotel_name)
+                //        {
+                //            return 0;
+                //        }
+                //    }
+                //    return 1;
+                //}
+                //  var listHotel = context.hotels.Where(h => h.location.id == int.Parse(location_id)).ToList();
+                //var listHotel = context.hotels.Where(h => h.location_id == a).ToList();
+
+                //foreach (var it in listHotel)
+                //{
+                //    if (it.hotel_name == hotel_name)
+                //    {
+                //        return 0;
+                //    }
+                //}
+            }
+            return 1;
+        }
+
+
+        //public ActionResult AddHotel(hotel hotel)
+        //{
+        //    var result = 
+        //    return View();
+        //}
     }
 }
---
+++ CNW_N8_MVC/Scripts/js/EditHotel.js
@@ -0,0 +1,177 @@
+﻿$(document).ready(function () {
+    $("#btnThem").attr('type', 'button');
+})
+
+$("#hotel_namee").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
+
+$("#description").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
+
+$("#more_imformation").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
+
+$("#price").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
+
+$("#sell_price").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
+
+$("#location_id").blur(function () {
+    var location_id = $("#location_id").val();
+    var hotel_name = $("#hotel_namee").val();
+    var description = $("#description").val();
+    var more_imformation = $("#more_imformation").val();
+    var price = $("#price").val();
+    var sell_price = $("#sell_price").val();
+
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/checkEditHotel",
+        data: {
+            location_id: location_id, hotel_name: hotel_name, description: description, more_imformation: more_imformation,
+            price: price, sell_price: sell_price
+        }
+    }).done(function (thongbao) {
+        if (thongbao == 0) {
+            $("#divThongBao").html("Tên khách sạn bị trùng");
+            $("#btnThem").attr('type', 'button');
+        } else if (thongbao == -1) {
+            $("#divThongBao").html("Thiếu Thông Tin");
+            $("#btnThem").attr('type', 'button');
+        } else {
+            $("#divThongBao").html("");
+            $("#btnThem").attr('type', 'submit');
+        }
+    });
+})
