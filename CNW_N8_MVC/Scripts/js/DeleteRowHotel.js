dab3515b34c81cb8642a3da68443213297c7d79b
hothang2407@gmail.com <hothang2407@gmail.com>
2020-06-21 16:20:16 +0700

    add anh

---
+++ CNW_N8_MVC/Scripts/js/DeleteRowHotel.js
@@ -0,0 +1,11 @@
+﻿function deleteRowHotel(id) {
+    $.ajax({
+        method: "post",
+        url: "/BackendHotel/DeleteHotel",
+        data: {
+            id: id
+        }
+    }).done(function(thongbao) {
+        $("#row_" + id).css('display', 'none');
+    });
+}
